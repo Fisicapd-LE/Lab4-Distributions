b4c70bcccb7867780c78005ace97c6ba4bbc3e74
Fisica2014-DC <davide.ch@hotmail.it>
2017-01-24 15:19:25 +0100

    added not working code, new version

--- other/per_enrico/getHistoLAB.C
+++ other/per_enrico/getHistoLAB.C
@@ -45,28 +45,38 @@ TH1F* getHistoWithFilter(char *name_file, int numBins, double minX, double maxX,
   TH1F *h_spectrum1 = new TH1F("h_spectrum1","Total spectrum1",numBins,minX,maxX);
   
   int k = 0;	//new index for cycles
+  bool trigger = 1;	//trigger == TRUE
+  
   
   // histogram filling
   for (int i=0; i<inbranch0->GetEntries(); i++) {	//cycle over R1 data
     inbranch0->GetEntry(i);
     if (indata0.qlong>lowThr0 && indata0.qlong<highThr0)	//the single energy measurement is in the peak 
     {
+      trigger = 1;
       //cout << "DEBUG: timetag0 = " << indata1.timetag << endl;
-      while(indata1.timetag < indata0.timetag - 500  ||  indata1.timetag > indata0.timetag +500)		//find the coincidence
+      while(indata1.timetag < indata0.timetag)		//find the coincidence
       {
-	if(k > inbranch1->GetEntries()) 
-	{
-	  cout << "Corresponding timetag not found!" << endl;
-	  break; 
-	}
-	if(k < 100)
-	{
-	  //cout << "DEBUG: timetag at k = " << k << " is " << indata1.timetag << endl;
-	}
+	//if(k > inbranch1->GetEntries() -1)
+	//{
+	  //cout << "FILE OVER = " << k << " is " << indata1.timetag << endl;
+	  //break;
+	//}
 	inbranch1->GetEntry(k);
 	k++;
       }
-      if(indata1.qlong>lowThr1 && indata1.qlong<highThr1)
+      if(indata1.timetag > (indata0.timetag + 100)) 
+      {
+	//cout << "DEBUG: timetag0 = " << indata0.timetag << " timetag1 = " << indata1.timetag << endl;
+	//cout << "DEBUG: condition = " << (indata1.timetag > indata0.timetag + 100) << endl;
+	//cout << "DEBUG: indata0.timetag + 100 = " << indata0.timetag + 100 << endl;
+	//cout << "DEBUG: distance " << indata0.timetag +100 - indata1.timetag << endl;
+	cout << "Corresponding timetag not found!" << endl;
+	trigger = 0;
+	k--;
+	continue; 
+      }
+      if(indata1.qlong>lowThr1 && indata1.qlong<highThr1 && trigger)
       {
 	cout << "Coincidence Found!" << endl;
         h_spectrum0->Fill(indata0.qlong);
